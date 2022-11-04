9c9a59baf6415cbc12840d04844c613854e077ff
João Pedro <medeirosjpro@hotmail.com>
2022-11-04 16:24:14 -0300

    Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -15,15 +15,26 @@ namespace SalesWebMvc.Services
         {
             _context = context;
         }
+        public List<Seller> FindaAll()
+        {
+            return _context.Seller.ToList();
+        }
         public void Insert(Seller obj)
         {
             _context.Add(obj);
             _context.SaveChanges();
         }
-
-        public List<Seller> FindaAll()
+        public Seller FindById(int id)
         {
-            return _context.Seller.ToList();
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
         }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Remove(obj);
+            _context.SaveChanges();
+        }
+
+
     }
 }

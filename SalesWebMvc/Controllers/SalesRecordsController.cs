8ffb4962b619a46d07bbe11f590184c039d2e80c
João Pedro <medeirosjpro@hotmail.com>
2022-11-10 10:38:19 -0300

    Implementing grouping search

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -25,19 +26,38 @@ namespace SalesWebMvc.Controllers
             if (!minDate.HasValue)
             {
                 minDate = new DateTime(DateTime.Now.Year, 1, 1);
+                //Caso não tenha valor na pesquisa, irá fixar em 01/01/atual
             }
             if (!maxDate.HasValue)
             {
                 maxDate = DateTime.Now;
+                //Caso não tenha valor na pesquisa, irá fixar na data atual
             }
             ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
             ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            //Tudo acima é para continuar mostrando a data selecionada na barra após a pesquisa-> View
             var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            //Chamada Assincrona para pesquisar as vendas
             return View(result);
         }
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+                //Caso não tenha valor na pesquisa, irá fixar em 01/01/atual
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+                //Caso não tenha valor na pesquisa, irá fixar na data atual
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            //Tudo acima é para continuar mostrando a data selecionada na barra após a pesquisa-> View
+            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            //Chamada Assincrona para pesquisar as vendas
+            return View(result);
         }
     }
 }
--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -11,6 +11,7 @@ namespace SalesWebMvc.Services
     public class SalesRecordService
     {
         private readonly SalesWebMvcContext _context;
+        //Faz a ligação com o banco de dados
 
         public SalesRecordService(SalesWebMvcContext context)
         {
@@ -23,16 +24,43 @@ namespace SalesWebMvc.Services
             if (minDate.HasValue)
             {
                 result = result.Where(x => x.Date >= minDate.Value);
+                //Caso haja valor, procura ele se a data for igual ou maior da data minima
             }
             if (maxDate.HasValue)
             {
                 result = result.Where(x => x.Date <= maxDate.Value);
+                //Caso haja valor, procura ele se a data for igual ou menor da data maxima
             }
             return await result
                 .Include(x => x.Seller)
                 .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
+            //Retorna o resultado assincrono em uma lista incluindo o vendedor, departamento e organizando da maior data pra menor
+        }
+
+        //Por utilizar a operação GroupBy, o retorno será agrupado em uma coleção IGrouping
+        //Então será uma Task de Lista de IGrouping de Departament e SalesRecord
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj; //pega o salesrecord do tipo dbset e construir um obj result do tipo IQuearyable
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+                //Caso haja valor, procura ele se a data for igual ou maior da data minima
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+                //Caso haja valor, procura ele se a data for igual ou menor da data maxima
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department) //Agrupa o resultado por Departamento
+                .ToListAsync();
+            //Retorna o resultado assincrono em uma lista incluindo o vendedor, departamento e organizando da maior data pra menor
         }
     }
 }
